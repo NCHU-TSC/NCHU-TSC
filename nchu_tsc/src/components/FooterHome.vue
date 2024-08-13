af9a2953a09f8f94899ce158726ff074a7308ab8
NullBlank <60308960+Frank2002code@users.noreply.github.com>
2024-08-13 20:52:56 +0800

    change to Vue.js

---
+++ nchu_tsc/src/components/FooterHome.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="p-4 border-top bg-body-tertiary">
+      <div class="container align-items-center justify-content-between d-flex">
+        <p class="mb-0 text-muted text-center d-flex align-items-center">
+          <span class="align-middle">© 2023 National Chung Hsing University Tutor Club</span>
+        </p>
+        <ul class="nav">
+          <li class="nav-item">
+            <a href="contact.htm" class="nav-link text-muted">聯絡我們</a>
+          </li>
+          <li class="nav-item">
+            <a href="about.htm" class="nav-link text-muted">關於本社</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link text-muted">Cookies</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link text-muted">資料收集</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FooterHome',
+  };
+  </script>
+  
+  <style scoped>
+  /* 這裡可以添加特定於這個組件的樣式 */
+  </style>
+  
